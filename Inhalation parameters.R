c02686b3f296272729d4ada859c4dca65b6baba7
JJLugt <j.j.vanderlugt@students.uu.nl>
2022-06-27 17:16:05 +0200

    Made some spelling corrections and updated the file to have to correct compartment names as used in the inhalation model

--- Inhalation parameters.R
+++ Inhalation parameters.R
@@ -1,5 +1,5 @@
 #author: Joris Jean van der Lugt
-#date: 20-05-2021
+#date: 23-06-2022
 #Inhalation parameters 
 library(RxODE)
 library(tidyverse)
@@ -11,55 +11,55 @@ library(reshape2)
 
 #Simulations
 set.seed(15204)         #to ensure a reproducible output
-amount.units <-"umol"
-time.units   <-"h"
-nbr.doses    <-1        #number of doses
-time.0       <-0        #time start dosing
-time.end     <-8        #time end of simulation
-time.frame   <-0.01     #time steps of simulation
-oral_dose_in_mg   <-0    #Dose in mg/kg-bw
-inhalation_dose_in_mg <- 100  #The inhaled dose in mg/kg
-MW           <-132.16   #The molecular weight of Cinnamaldehyde
-DOSE         <-(oral_dose_in_mg * 70)/ MW  * 1e+3     #The administered dose in umol 
-INHALED_DOSE <-(inhalation_dose_in_mg * 70)/ MW  * 1e+3
-Volume_exposure_chamber <- 10  #volume exposure chamber in L
-C_chamber <- INHALED_DOSE/Volume_exposure_chamber
+amount.units            <-"umol"
+time.units              <-"h"
+nbr.doses               <-1        #number of doses
+time.0                  <-0        #time start dosing
+time.end                <-8        #time end of simulation
+time.frame              <-0.01     #time steps of simulation
+oral_dose_in_mg         <-100    #Dose in mg/kg-bw
+inhalation_dose_in_mg   <-100  #The inhaled dose in mg/kg
+MW                      <-132.16   #The molecular weight of Cinnamaldehyde
+DOSE                    <-(oral_dose_in_mg * 70)/ MW  * 1e+3     #The administered dose in umol 
+INHALED_DOSE            <-(inhalation_dose_in_mg * 70)/ MW  * 1e+3 
+Volume_exposure_chamber <-10  #volume exposure chamber in L
 
 #--Physico-chemical parameters--#
 #-Cinnamaldehyde-#
 
-P_F      <-  39.3 #Fat/Blood partition coefficient
-P_L      <-  2.04 #liver/Blood partition coefficient
-P_SI     <-  2.04 #Small intestine/Blood partition coefficients
-P_RP     <-  2.04 #Richly perfused tissues/Blood partition coefficients
-P_SP     <-  1.57 #Slowely perfused tissues/Blood partition coefficients
-P_B      <- 274.84 # Blood/Air Partition Coefficient 
-P_P     <-  2.04  #lung/Blood partition coefficient
+P_F      <-  39.3  #Fat/Blood partition coefficient
+P_L      <-  2.04  #liver/Blood partition coefficient
+P_SI     <-  2.04  #Small intestine/Blood partition coefficients
+P_RP     <-  2.04  #Richly perfused tissues/Blood partition coefficients
+P_SP     <-  1.57  #Slowly perfused tissues/Blood partition coefficients
+P_B      <- 274.84 #Blood/Air Partition Coefficient 
+P_Pu      <-  2.04  #lung/Blood partition coefficient
 
-#-Cinnamyl AlcOHol-#
+#-Cinnamyl Alcohol-#
 P_OH_F    <-  40.5 #Fat/Blood partition coefficient
 P_OH_L    <-  2.09 #liver/Blood partition coefficient
 P_OH_SI   <-  2.09 #Small intestine/Blood partition coefficients
 P_OH_RP   <-  2.09 #Richly perfused tissues/Blood partition coefficients
 P_OH_SP   <-  1.60 #Slowly perfused tissues/Blood partition coefficients
-P_OH_P   <-  2.09 #Lung/Blood partition coefficients
-#--Pyshiological Parameters--#
-BW      <- 70    #Body weight in Kg
+P_OH_Pu    <-  2.09 #Lung/Blood partition coefficients
+
+#--Physiological Parameters--#
+BW        <- 70    #Body weight in Kg
 
 #-Tissues volumes in % body weight-#
 
-V_F      <- 21.4  #Fat
-V_L      <- 2.6   #Liver
-V_SI     <- 0.9   #Small intestine
-V_A      <- 2.0   #Arterial Blood
-V_V      <- 5.9   #Venous Blood
-V_RP     <- 3.3   #Richly perfused 
-V_SP     <- 51.7  #Slowly perfused 
-V_P     <- 0.8   #Lung volume 
+V_F       <- 21.4  #Fat
+V_L       <- 2.6   #Liver
+V_SI      <- 0.9   #Small intestine
+V_A       <- 2.0   #Arterial Blood
+V_V       <- 5.9   #Venous Blood
+V_RP      <- 3.3   #Richly perfused 
+V_SP      <- 51.7  #Slowly perfused 
+V_Pu      <- 0.8   #Lung volume 
 
 #-Cardiac parameters-#
 
-Q_C      <- 310    #Cardiac output in L/h
+Q_C       <- 310  #Cardiac output in L/h
 
 #-Blood flow to tissues in % cardiac output-#
 
@@ -68,14 +68,13 @@ Q_L      <- 14.1   #Liver
 Q_SI     <- 8.6    #Small intestine
 Q_RP     <- 47.3   #Richly perfused (RP)
 Q_SP     <- 24.8   #Slowly perfused (SP)
-Q_P     <- 100    #lung blood flow 
 
 
 #inhalation parameters
-Q_PV <- 300  # aveolar ventilation l/h
+Q_PV     <- 300    #Alveolar ventilation l/h
 
 #----GSH parameters----#
-#--GSH synthesis in umol/kg tissue/h--#
+#--GSH synthesis in μmol/kg tissue/h--#
 
 G_SYN_L     <- 1122  #Liver 
 G_SYN_SI    <- 27    #Small intestine
@@ -85,19 +84,19 @@ k_L_GLOS    <- 0.142 #Liver
 k_SI_GLOS   <- 0.044 #Small intestine
 
 #--Initial GSH concentration--#
-init_GSH_L  <- 5639 * V_L   #initial amount of GSH in the liver in umol/kg
-init_GSH_SI <- 1250 * V_SI  #initial amount of GSH in the small intestine in umol/kg
+init_GSH_L  <- 5639 * V_L   #initial amount of GSH in the liver in μmol/kg
+init_GSH_SI <- 1250 * V_SI  #initial amount of GSH in the small intestine in μmol/kg
 
-k_GSH <- 6.6 * 10^(-4) #The second-order rate constant of the chemical reaction of cinnamaldehyde with GSH in μmol/h
-k_DNA <- 1.6 * 10^(-8) #The second-order rate constant of the reaction between cinnamaldehyde and 2ʹ-dG in μmol/h
+k_GSH <- 6.6 * 10^(-4)   #The second-order rate constant of the chemical reaction of cinnamaldehyde with GSH in μmol/h
+k_DNA <- 1.6 * 10^(-8)   #The second-order rate constant of the reaction between cinnamaldehyde and 2ʹ-dG in μmol/h
 
 #----Protein reactive sites in μmol/kg tissue----#
-C_PRO_L     <- 5319  #Liver
-C_PRO_SI    <- 245   #Small intestine
+C_PRO_L     <- 5319      #Liver
+C_PRO_SI    <- 245       #Small intestine
 
 #----DNA parameters----#
-C_L_dG     <-  1.36 #Concentration of 2ʹ-dG in the liver μmol/kg liver
-T_0.5      <-  38.5   #Half-life of DNA adduct in the liver in hours
+C_L_dG     <-  1.36      #Concentration of 2ʹ-dG in the liver μmol/kg liver
+T_0.5      <-  38.5      #Half-life of DNA adduct in the liver in hours
 
 
 #--Chemical parameters--#
@@ -106,18 +105,18 @@ Ka <- 5.0  #Absorption rate constant for uptake in the Small intestine in per H
 #----Liver----#
 
 #-first rate order constants-#
-k_L_OH  <- 4.2e-02   #Scaled first rate order constant for the enzymatic oxidation of cinnamyl alcohol in the liver in umol/h
+k_L_OH  <- 4.2e-02       #Scaled first rate order constant for the enzymatic oxidation of cinnamyl alcohol in the liver in umol/h
 
 #--Michaelis menten constants--#
-Km_L_CA     <-  8.5  #Km for enzymatic oxidation of cinnamaldehyde into Cinnamic acid in the liver in μM
-Km_L_AO     <-  330  #Km for enzymatic reduction of cinnamaldehyde into cinnamyl alcOHol in the liver in μM
-Km_L_GST    <-  100 #Km for enzymatic conjugation of cinnamaldehyde with GST in the liver in μM  
+Km_L_CA     <-  8.5      #Km for enzymatic oxidation of cinnamaldehyde into Cinnamic acid in the liver in μM
+Km_L_AO     <-  330      #Km for enzymatic reduction of cinnamaldehyde into cinnamyl alcOHol in the liver in μM
+Km_L_GST    <-  100      #Km for enzymatic conjugation of cinnamaldehyde with GST in the liver in μM  
 Km_L_GST_G  <-  1.7*10^3 #Km toward GSH for enzymatic conjugation of cinnamaldehyde in the liver (μM)
 
 #--Vmax values--#
-Vsmax_L_CA    <-  9.7  #Scaled Vmax for enzymatic oxidation of cinnamaldehyde in the liver in μmol/h 
-Vsmax_L_AO    <-  73   #Scaled Vmax for enzymatic reduction of cinnamaldehyde in the liver in μmol/h
-Vsmax_L_GST   <-  37   #Scaled Vmax for enzymatic conjugation of cinnamaldehyde with GSH in the liver in μmol/h
+Vsmax_L_CA    <-  9.7    #Scaled Vmax for enzymatic oxidation of cinnamaldehyde in the liver in μmol/h 
+Vsmax_L_AO    <-  73     #Scaled Vmax for enzymatic reduction of cinnamaldehyde in the liver in μmol/h
+Vsmax_L_GST   <-  37     #Scaled Vmax for enzymatic conjugation of cinnamaldehyde with GSH in the liver in μmol/h
 
 #----Small intestines----#
 #--Michaelis menten constants--#
@@ -125,13 +124,13 @@ Km_SI_CA    <- 70  #Km for enzymatic oxidation of cinnamaldehyde into cinnamic a
 Km_SI_AO    <- 90  #Km for enzymatic reduction of cinnamaldehyde into cinnamyl alcOHol in the Small Intestine in μM
 Km_SI_OH    <- 290 #Km for enzymatic oxidation of cinnamly alcOHol into cinnamaldehyde in the Small Intestine in μM
 Km_SI_GST   <- 600 #Km for enzymatic conjugation of cinnamaldehye with GST in the Small Intestine in μM (RAT value)
-Km_SI_GST_G <- 0  #Km toward GSH for enzymatic conjugation of cinnamaldehyde in the small intestine (μM)
+Km_SI_GST_G <- 0   #Km toward GSH for enzymatic conjugation of cinnamaldehyde in the small intestine (μM)
 
 #-Vmax values-#
-Vsmax_SI_CA    <- 21 #Scaled Vmax for enzymatic oxidation of cinnamaldehyde into Cinnamic acid in the Small Intestine in μmol/h 
-Vsmax_SI_AO    <- 30 #Scaled Vmax for enzymatic reduction of cinnamaldehyde into Cinnamyl alcOHol in  the Small Intestine in μmol/h 
+Vsmax_SI_CA    <- 21  #Scaled Vmax for enzymatic oxidation of cinnamaldehyde into Cinnamic acid in the Small Intestine in μmol/h 
+Vsmax_SI_AO    <- 30  #Scaled Vmax for enzymatic reduction of cinnamaldehyde into Cinnamyl alcOHol in  the Small Intestine in μmol/h 
 Vsmax_SI_OH    <- 5.0 #Scaled Vmax for enzymatic Oxidation of cinnamyl alcohol into cinnamaldehyde in the Small Intestine in μmol/h 
-Vsmax_SI_GST   <- 63 #Scaled Vmax for enzymatic Conjugation of cinnamaldehyde with GSH in the in the small intestine in μmol/h (RAT value)
+Vsmax_SI_GST   <- 63  #Scaled Vmax for enzymatic Conjugation of cinnamaldehyde with GSH in the in the small intestine in μmol/h (RAT value)
 
 #Collection of all parameters so they can be entered in the function
 parameters=cbind(Volume_exposure_chamber,
@@ -141,13 +140,13 @@ parameters=cbind(Volume_exposure_chamber,
                  P_RP,
                  P_SP,
                  P_B,
-                 P_P,
+                 P_Pu,
                  P_OH_F,
                  P_OH_L,
                  P_OH_SI,
                  P_OH_RP,
                  P_OH_SP,
-                 P_OH_P,
+                 P_OH_Pu,
                  BW,
                  V_F,
                  V_L,
@@ -156,14 +155,13 @@ parameters=cbind(Volume_exposure_chamber,
                  V_V,
                  V_RP,
                  V_SP,
-                 V_P,
+                 V_Pu,
                  Q_C,
                  Q_F,
                  Q_L,
                  Q_SI,
                  Q_RP,
                  Q_SP,
-                 Q_P,
                  Q_PV,
                  G_SYN_L,
                  G_SYN_SI,
@@ -201,9 +199,9 @@ parameters=cbind(Volume_exposure_chamber,
 
 #defining the begin situation of the model Inhalation variation 
 inits <- c("A_GI"         =0,
-           "A_I"          =0,
-           "A_PU"         =0,
-           "A_OH_PU"      =0 ,
+           "A_P_Art"      =0,
+           "A_Pu"         =0,
+           "A_OH_Pu"      =0 ,
            "A_V"          =0,
            "A_OH_V"       =0,
            "A_F"          =0,
@@ -240,5 +238,5 @@ inits <- c("A_GI"         =0,
 #inhalation exposure  exposure
 ex <- eventTable(amount.units = amount.units, time.units = time.units) %>%
   et(dose = DOSE, dur=0.01, cmt="A_GI", nbr.doses=nbr.doses)%>%
-  et(dose = C_chamber, dur = 0.01, cmt="A_I", nbr.doses=nbr.doses)%>%
+  et(dose = INHALED_DOSE, dur = 0.01, cmt="C_inhaled", nbr.doses=nbr.doses)%>%
   et(seq(from = time.0, to = time.end, by = time.frame)) 
